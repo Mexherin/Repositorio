b71d6abe909bccaf433a667e793df5e5ecb696f0
David Cruzado Sias <cruzadosiasd@gmai.com>
2026-09-05 11:23:23 -0500

    Corrige contenido de main.py con saludos del equipo

---
+++ main.py
@@ -0,0 +1,8 @@
+def saludar(nombre, mensaje):
+    print(f"Hola, soy {nombre}. {mensaje}")
+saludar("Carlos Gonzales", "Hasta aqui mi participación señores")
+saludar("Danny Pinto", "Un gusto caramelito muchachos")
+saludar("David Cruzado", "Qué pasa guaneros?")
+saludar("Fabian Huaman", "Me parece útil el control de versiones")
+saludar("Jhordan Guevara", "Trabajar en equipo es mejor")
+#LOS SALUDOS DEL GRUPO
